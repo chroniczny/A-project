a6073525cbc892035e3da58d53052eb675310dca
Jakub Kwiatkowski (PGS Software) <jkwiatkowski@pgs-soft.com>
2017-01-17 07:32:46 +0100

    commit before merging and pulling

--- app/app.config.js
+++ app/app.config.js
@@ -1,21 +1,21 @@
-// 'use strict';
-//
-// angular.
-// module('app', ['ngRoute']).
-// config(['$locationProvider', '$routeProvider',
-//     function config($locationProvider, $routeProvider) {
-//         $locationProvider.hashPrefix('!');
-//
-//         $routeProvider.
-//         when('/users', {
-//             template: '<user-list></user-list>'
-//         }).
-//         when('/users/:usersId', {
-//             template: '<user-detail></user-detail>'
-//
-//         }).
-//         otherwise('/users');
-//     }
-// ]);
+'use strict';
+
+angular.
+module('app', ['ngRoute']).
+config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+
+        $routeProvider.
+        when('/users', {
+            template: '<user-list></user-list>'
+        }).
+        when('/users/:usersId', {
+            template: '<user-detail></user-detail>'
+
+        }).
+        otherwise('/users');
+    }
+]);
 
 
--- app/user-detail/user-detail.component.js
+++ app/user-detail/user-detail.component.js
@@ -7,10 +7,10 @@
 angular.module('userDetail')
     .component('userDetail', {
         templateUrl: 'user-detail/user-detail.template.html',
-        // template: 'Something new - a new detail <span>{{$ctrl.users.id}}</span>', not working {{}}
+        // template: <
         controller: ['$http', '$routeParams',
         function UserDetailController($http, $routeParams){
-            // this.userId = $routeParams.userId;
+            this.userId = $routeParams.userId;
             var self = this;
 
             $http.get('users/'+$routeParams.userId+'.json')
