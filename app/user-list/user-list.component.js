5a9e0d8c3dbfffb191f5676fb1c00ac4c4f1588c
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2017-01-19 23:17:47 +0100

    sketches to deleting array of Users by del-click

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -8,69 +8,90 @@ angular.module('app')
         templateUrl: 'user-list/user-list.template.html',
 
         controller: //  --- before SERVICE and FACTORY ----
-        // ['$http',
-        // function UserListController($http) {
-        //     var self = this;
-        //     self.sortProp = 'name.first'; // filter: sorting results of searching by first name
-        //
-        //     self.searchValue = '';
-        //
-        //     self.searchUser = function searchUser() {
-        //         self.filterProp = self.searchValue;
-        //     };
-        //
-        //     $http.get('users/users.json').then(function(response){
-        //         //self.users = response.data; // --> .slice(0,5); ale potem nie wyszukuje wśród wszsytkich
-        //
-        //          // setting data to localStorage in JSON format
-        //     localStorage.setItem('UserInStorage', JSON.stringify(response.data));
-        //
-        //          //viewing full localStorage in console
-        //     // console.log(localStorage.getItem('UserInStorage'));
-        //
-        //         // taking viewing data for templates by using localStorage data
-        //     self.users = JSON.parse(localStorage.getItem('UserInStorage'));
-        //     });
-        // }
-        // ]
+        ['$http',
+        function UserListController($http) {
+             var self = this;
+             self.sortProp = 'name.first'; // filter: sorting results of searching by first name
+
+             self.searchValue = '';
+
+             self.searchUser = function searchUser() {
+                 self.filterProp = self.searchValue;
+             };
+
+             $http.get('users/users.json').then(function(response){
+                 //self.users = response.data; // --> .slice(0,5); ale potem nie wyszukuje wśród wszsytkich
+
+                  // setting data to localStorage in JSON format
+             localStorage.setItem('UserInStorage', JSON.stringify(response.data));
+
+                  //viewing full localStorage in console
+             // console.log(localStorage.getItem('UserInStorage'));
+
+                 // taking viewing data for templates by using localStorage data
+             self.users = JSON.parse(localStorage.getItem('UserInStorage'));
+             });
+
+            self.deleteUser = function deleteUser(user){
+                var newUsers = [];
+                var filterinUsers = self.users;
+
+                for (var i = 0; i<filterinUsers.length; i++){
+                    if (filterinUsers[i].id != user.id) {
+                        newUsers.push(filterinUsers[i]);
+                    } else {
+                        console.log(filterinUsers[i].id);
+                    }
+                 }
+
+                localStorage.setItem('newUsers', JSON.stringify(newUsers));
+                console.log(localStorage.getItem('newUsers'));
+                // filterinUsers = newUsers;
+
+            }
+
+
+
+        }
+        ]
 
         // ------------ using service and factory ----
-            ['Userrr',
-                function UserListController(Userrr) {
-                    var self = this;
-
-                    self.sortProp = 'name.first'; // filter: sorting results of searching by first name
-                    self.searchValue = '';
-                    self.searchUser = function searchUser() {
-                        self.filterProp = self.searchValue;
-                    };
-
-                    // take data from json-files and set it to the localStorage by ng-click
-                    // self.dataHunt = function dataHunt() {
-                        // set the data from factory Userrr.query() to the local storage
-                        // var huntedUsers = Userrr.query();
-                        // localStorage.setItem('UserInStorage', JSON.stringify(Userrr.query()));
-                        // view it in console:
-                        // console.log(localStorage.getItem('UserInStorage')); // GIVES ME [] as a Promise
-                    // };
-
-                    // console.log(JSON.stringify(Userrr.query())); // again I see [] in console
-
-                    // for templates taking data from localStorage
-                    // self.users = JSON.parse(localStorage.getItem('UserInStorage'));
-
-                    /////// --- lets see again:
-
-                    self.users = Userrr.query();
-
-
+        //    ['Userrr',
+        //        function UserListController(Userrr) {
+        //            var self = this;
+        //
+        //            self.sortProp = 'name.first'; // filter: sorting results of searching by first name
+        //            self.searchValue = '';
+        //            self.searchUser = function searchUser() {
+        //                self.filterProp = self.searchValue;
+        //            };
+        //
+        //            // take data from json-files and set it to the localStorage by ng-click
+        //            // self.dataHunt = function dataHunt() {
+        //                // set the data from factory Userrr.query() to the local storage
+        //                // var huntedUsers = Userrr.query();
+        //                // localStorage.setItem('UserInStorage', JSON.stringify(Userrr.query()));
+        //                // view it in console:
+        //                // console.log(localStorage.getItem('UserInStorage')); // GIVES ME [] as a Promise
+        //            // };
+        //
+        //            // console.log(JSON.stringify(Userrr.query())); // again I see [] in console
+        //
+        //            // for templates taking data from localStorage
+        //            // self.users = JSON.parse(localStorage.getItem('UserInStorage'));
+        //
+        //            /////// --- lets see again:
+        //
+        //            self.users = Userrr.query();
 
 
 
 
 
 
-                }]
+
+
+                //}]
 
 
     });
